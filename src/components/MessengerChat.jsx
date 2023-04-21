8315776a2046d9ac8629f9a2109942dfcca30690
Turya Biswas <78296735+turyabiswas233@users.noreply.github.com>
2023-04-21 15:06:01 +0600

    roll back and changed

--- src/components/MessengerChat.jsx
+++ src/components/MessengerChat.jsx
@@ -1,8 +1,19 @@
 import React from "react";
-import MessengerCustomerChat from "react-messenger-customer-chat";
 
 const MessengerChat = () => {
-  return <MessengerCustomerChat pageId="100959359657207" />;
+  return (
+    <a href="https://m.me/100959359657207" target="_blank">
+      <div
+        className="fb-customerchat"
+        id="fb-customer-chat"
+        attribution="biz_inbox"
+        page_id="100959359657207"
+        theme_color="#7750DD"
+      >
+        <img src="/src/assets/tools/msg.png" width={40} height={40} alt="msg" />
+      </div>
+    </a>
+  );
 };
 
 export default MessengerChat;
