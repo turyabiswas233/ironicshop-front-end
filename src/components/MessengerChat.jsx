6b713566e5513474ba0892009d97549bac94c119
Turya Biswas <78296735+turyabiswas233@users.noreply.github.com>
2023-04-21 16:55:28 +0600

    bug fix -- update admin

--- src/components/MessengerChat.jsx
+++ src/components/MessengerChat.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import msg from "/src/assets/tools/msg.png";
 const MessengerChat = () => {
   return (
     <a href="https://m.me/100959359657207" target="_blank">
@@ -10,7 +10,7 @@ const MessengerChat = () => {
         page_id="100959359657207"
         theme_color="#7750DD"
       >
-        <img src="/src/assets/tools/msg.png" width={40} height={40} alt="msg" />
+        <img src={msg} width={40} height={40} alt="msg" />
       </div>
     </a>
   );
